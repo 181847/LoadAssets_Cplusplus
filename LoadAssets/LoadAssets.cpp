19a020ef27ceadb78ee0ac75fd5bd7cbd28e947a
181847 <753090084@qq.com>
2017-11-10 20:01:36 +0800

    测试添加一个普通lua函数

--- LoadAssets/LoadAssets.cpp
+++ LoadAssets/LoadAssets.cpp
@@ -5,13 +5,26 @@
 #include "LuaInterpreter.h"
 #include <memory>
 
+static int addfunction(lua_State * L)
+{
+	int n1 = luaL_checkinteger(L, 1);
+	int n2 = luaL_checkinteger(L, 2);
+	lua_pushinteger(L, n1 + n2);
+	return 1;
+}
+
 
 int main()
 {
 	std::unique_ptr<LuaInterpreter> luaInter = std::make_unique<LuaInterpreter>();
 
-	luaInter->Run();
+	luaInter->m_L;
 
+	lua_pushcfunction(luaInter->m_L, addfunction);
+
+	lua_setglobal(luaInter->m_L, "add");
+
+	luaInter->Run();
 
 	
 	fprintf(stdout, "Program terminate.");
