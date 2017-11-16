401a37a29033572a635cb47de2d4efe722b8a197
181847 <753090084@qq.com>
2017-11-17 00:01:51 +0800

    测试贴图加载。

--- LoadAssets/LoadAssets.cpp
+++ LoadAssets/LoadAssets.cpp
@@ -3,18 +3,26 @@
 
 #include "stdafx.h"
 #include "LuaInterpreter.h"
-#include <memory>
+#include "lib/DirectX12/FrameResource.h"
+#include "AssetsTools.h"
 
 void execute()
 {
 	std::unique_ptr<LuaInterpreter> luaInter = std::make_unique<LuaInterpreter>();
 
+	// call init lua to start assets loading.
 	int error = luaL_loadfile(luaInter->m_L, "Init.lua") || lua_pcall(luaInter->m_L, 0, 0, 0);
 	
-	ASSERT(Not(error))
+	ASSERT(Not(error));
+
+	// The map to store the material data.
+	std::unordered_map<std::string, Material> globalMaterial;
+	
+	// Load all the material int the map,
+	// the name will be the key.
+	LuaLoadMaterial(luaInter.get(), &globalMaterial);
 
 	luaInter->Run();
-
 
 	fprintf(stdout, "Program terminate.");
 	getchar();
