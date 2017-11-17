c4431eec1f1f840a6b12b62734a8cd342fc7df6c
181847 <753090084@qq.com>
2017-11-17 09:48:36 +0800

    完善获取材质信息的部分

--- LoadAssets/AssetsTools.cpp
+++ LoadAssets/AssetsTools.cpp
@@ -1,5 +1,11 @@
 #include "AssetsTools.h"
 
+#ifdef _DEBUG
+#define	ShowStackSize(luaInterpreter) printf("StackSize: %d\n", luaInterpreter->GetStackSize())
+#else
+#define ShowStackSize(luaInterpreter)
+#endif
+
 bool LuaLoadMaterial(LuaInterpreter * pLuaInter, std::unordered_map<std::string, Material> * matMap)
 {
 	ASSERT(pLuaInter);
@@ -60,27 +66,104 @@ bool LuaLoadSingleMaterial(LuaInterpreter * pLuaInter, std::unordered_map<std::s
 {
 	ASSERT(pLuaInter);
 	ASSERT(matMap);
-	int checker;				// to check if the element is expected type.
-	KeyGenerator<32> formater;	// to generatoer the key string
+	int checker;							// to check if the element is expected type.
+	KeyGenerator<32> formater;				// to generatoer the key string
 
-	lua_State * L = pLuaInter->m_L;	// get for convience
+	lua_State * L = pLuaInter->m_L;			// get for convience
 
+	// ready to get string
+	const size_t StringMaxLength = 256;
+	size_t StringLen = 0;					// length of string in the lua
+	const char * TempString;				// obtain the string from lua
+
+	// get material name
+	char matName[StringMaxLength];
+	StringLen = 0;
+	lua_getfield(L, -1, "name");
+	TempString = lua_tolstring(L, -1, &StringLen);
+	ThrowIfFalse(StringLen > 0 && StringLen < StringMaxLength);
+	strcpy_s(matName, StringMaxLength, TempString);
+	lua_pop(L, 1);							// pop the name
+	printf("Material Name; %s\n", matName);
+
+	ShowStackSize(pLuaInter);
 	// get diffuseAlbedo, contain 4 number;
 	double dAlbe = 0;
 	lua_getfield(L, -1, "diffuseAlbedo");
-	int clearStackSize = 1;
-
+	// 4 diffuseAlbedo
 	for (int i = 1; i <= 4; ++i)
 	{
 		lua_geti(L, -1, i);
 		dAlbe = lua_tonumberx(L, -1, &checker);
 		ASSERT(checker);
 		printf("diffuseAlbedo[%d]: %lf\n", i, dAlbe);
-		lua_pop(L, 1);		// pop the number immediately
+		lua_pop(L, 1);						// pop the number immediately
 	}
+	lua_pop(L, 1);							// pop the diffuseAlbedo
 
-	// clear the stack, not include the material itself.
-	lua_pop(L, clearStackSize);
+	ShowStackSize(pLuaInter);
+	// 3 FresnelR
+	double fresnelR = 0;
+	lua_getfield(L, -1, "fresnelR");
+	for (int i = 1; i <= 3; ++i)
+	{
+		lua_geti(L, -1, i);
+		fresnelR = lua_tonumberx(L, -1, &checker);
+		ASSERT(checker);
+		printf("fresnelR[%d]: %lf\n", i, fresnelR);
+		lua_pop(L, 1);
+	}
+	lua_pop(L, 1);							// pop fresnelR
 
+	// roughness is just one number
+	double roughness = 0;
+	lua_getfield(L, -1, "roughness");
+	roughness = lua_tonumberx(L, -1, &checker);
+	printf("roughness: %lf\n", roughness);
+	lua_pop(L, 1);							// pop roughness
+
+
+	//diffuseMap
+	char diffuseMapName[StringMaxLength];
+	TempString = nullptr;
+	ShowStackSize(pLuaInter);
+	lua_getfield(L, -1, "diffuseMap");
+	ShowStackSize(pLuaInter);
+	// does the material contain any diffuseMap?
+	if (lua_isnil(L, -1)) // no
+	{
+		ShowStackSize(pLuaInter);
+		printf("No diffuse map.\n");
+	}
+	else //yes
+	{
+		ShowStackSize(pLuaInter);
+		TempString = lua_tolstring(L, -1, &StringLen);
+		ASSERT(TempString);
+		ThrowIfFalse(StringLen > 0 && StringLen < StringMaxLength);
+		strcpy_s(diffuseMapName, StringMaxLength, TempString);
+		printf("diffuseMap: %s\n", diffuseMapName);
+	}
+	lua_pop(L, 1);						// pop the diffuseMap(or the nil)
+
+	//normalMap
+	char normalMapName[256];
+	TempString = nullptr;
+	StringLen = 0;
+	lua_getfield(L, -1, "normalMap");
+	// does the material contain any normalMap?
+	if (lua_isnil(L, -1)) // no
+	{
+		printf("No normal map.\n");
+	}
+	else //yes
+	{
+		TempString = lua_tolstring(L, -1, &StringLen);
+		ASSERT(TempString);
+		ThrowIfFalse(StringLen > 0 && StringLen < StringMaxLength);
+		strcpy_s(normalMapName, StringMaxLength, TempString);
+		printf("normalMap: %s\n", normalMapName);
+	}
+	lua_pop(L, 1);					// pop the normalMap(or the nil)
 	return true;
 }
