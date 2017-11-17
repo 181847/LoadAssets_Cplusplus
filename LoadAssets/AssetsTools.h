f474056614dae25054f469f5071e92cd8d9b9cdb
181847 <753090084@qq.com>
2017-11-17 15:54:49 +0800

    修改函数声明，使用vector存储Material
    不再考虑在C++中存储字符串，所有字符串到对应资源的映射必须全部在lua中完成并确保正确。

--- LoadAssets/AssetsTools.h
+++ LoadAssets/AssetsTools.h
@@ -2,16 +2,17 @@
 #include "LuaInterpreter.h"
 #include "KeyGenerator.h"
 #include "lib\DirectX12\FrameResource.h"
-
+#include <functional>
 // This is a header which define many tool function to load assets,
 // the function whose name start with Lua mean the function need a LuaInterpreter,
 
 
+// ensure that the assembleSet is sitted at top of the stack.
 // Load Material Data from the luaInterpreter, 
 // the additional Material will be append into the matMap,
 // if there no confliction of name(or the old Material will be replace).
 bool LuaLoadMaterial(LuaInterpreter* pLuaInter,
-	std::unordered_map<std::string, Material> * matMap);
+	std::vector<Material> * matArr);
 
 
 
@@ -20,4 +21,6 @@ bool LuaLoadMaterial(LuaInterpreter* pLuaInter,
 // then clear the stack to where the top material
 // (the material also be popped).
 bool LuaLoadSingleMaterial(LuaInterpreter* pLuaInter,
-	std::unordered_map<std::string, Material> * matMap);
+	std::vector<Material> * matArr);
+
+void ShowDetail(Material & m);
