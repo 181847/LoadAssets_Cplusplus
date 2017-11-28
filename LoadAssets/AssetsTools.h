b42c12c47b0b0172bcc53adff3f5415f5ee33a98
181847 <753090084@qq.com>
2017-11-28 23:44:41 +0800

    修改头文件导入目录，注释到现在无法测试的函数，增加一个新的遍历函数（尚未实现）。

--- LoadAssets/AssetsTools.h
+++ LoadAssets/AssetsTools.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "../Library/Lua/LuaInterpreter/LuaInterpreter.h"
-#include "../Library/Lua/LuaModuls/LuaMeshData/LuaMeshDataStruct.h"
-#include "../Library/MyTools/Formater.h"
-#include "../Library/MyTools/LuaTools.h"
-#include "../Library/DirectX12/FrameResource.h"
-#include "../Library/DirectX12/Common/d3dUtil.h"
+#include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
+#include "../../Library/Lua/LuaModuls/LuaMeshData/LuaMeshDataStruct.h"
+#include "../../Library/MyTools/Formater.h"
+#include "../../Library/MyTools/LuaTools.h"
+#include "../../Library/DirectX12/FrameResource.h"
+#include "../../Library/DirectX12/Common/d3dUtil.h"
 
 #pragma comment(lib, "LuaInterpreter.lib")
 
@@ -36,11 +36,11 @@ bool LuaLoadSingleMaterial(LuaInterpreter* pLuaInter,
 	std::vector<Material> * matArr);
 
 // ensure that the assembleSet is on the top of the stack.
-bool
-LuaLoadGeometrys(LuaInterpreter* pLuaInter,
-	std::vector<std::unique_ptr<MeshGeometry>> *geoArr,	// store all the geometry
-	ID3D12Device * mDevice,
-	ID3D12GraphicsCommandList * mCmdList);
+//bool
+//LuaLoadGeometrys(LuaInterpreter* pLuaInter,
+//	std::vector<std::unique_ptr<MeshGeometry>> *geoArr,	// store all the geometry
+//	ID3D12Device * mDevice,
+//	ID3D12GraphicsCommandList * mCmdList);
 
 // ensure that the assembleSet is on the top of the stack,
 // here will use a function to convert the MeshData to MeshGeoemtry
@@ -56,6 +56,18 @@ LuaLoadGeometrys(LuaInterpreter* pLuaInter,
 // A debug function to show the Material data.
 void ShowDetail(Material & m);
 
+
+template<typename GEOMETRY>
+bool 
+LuaLoadGeometrys(
+	LuaInterpreter * pLuaInter, 
+	std::vector<std::unique_ptr<GEOMETRY>>* geoArr, 
+	std::function<std::unique_ptr<GEOMETRY>(std::string&name, Lua::MeshData*)> converter, 
+	std::function<void(std::string, UINT startIndex, UINT endIndex)> subMeshCollector)
+{
+	pLuaInter->GetFieldOnTop("GeometryQueue");
+	return false;
+}
 
 }// namespace LoadAssets
 }// Lua
