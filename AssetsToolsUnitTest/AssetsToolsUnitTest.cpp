dfd437d395fce9293131c85e283963f5eacaf4f2
181847 <753090084@qq.com>
2017-11-29 19:28:23 +0800

    添加一个对AssetsTools的测试项目，进行单独的测试。

---
+++ AssetsToolsUnitTest/AssetsToolsUnitTest.cpp
@@ -0,0 +1,13 @@
+// AssetsToolsUnitTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
+#include "../LoadAssets/AssetsTools.h"
+
+
+int main()
+{
+    return 0;
+}
+
