2db6daad6fdb5c1ea79273c6ea9faffcb47d9fac
181847 <753090084@qq.com>
2017-11-30 21:27:35 +0800

    资源增加测试用例，防止偶然的错误发生。

--- AssetsToolsUnitTest/luaScript/startLoadAssets.lua
+++ AssetsToolsUnitTest/luaScript/startLoadAssets.lua
@@ -11,14 +11,46 @@ test_t_1 = Texture.new("brick", "d:/texture/brick.dds")
 test_t_2 = Texture.new("tree", "d:/texture/tree.dds")
 
 test_m_1 = Material.new("baseMat")
+test_m_1.diffuseAlbedo[1] = 1.1
+test_m_1.diffuseAlbedo[2] = 1.2
+test_m_1.diffuseAlbedo[3] = 1.3
+test_m_1.diffuseAlbedo[4] = 1.4
+test_m_1.fresnelR[1] = 1.1
+test_m_1.fresnelR[2] = 1.2
+test_m_1.fresnelR[3] = 1.3
+test_m_1.roughness = 1.11
+test_m_1.normalMap = test_t_1.name
+
 test_m_2 = Material.new("whiteMat")
+test_m_2.diffuseAlbedo[1] = 2.1
+test_m_2.diffuseAlbedo[2] = 2.2
+test_m_2.diffuseAlbedo[3] = 2.3
+test_m_2.diffuseAlbedo[4] = 2.4
+test_m_2.fresnelR[1] = 2.1
+test_m_2.fresnelR[2] = 2.2
+test_m_2.fresnelR[3] = 2.3
+test_m_2.roughness = 2.11
+test_m_2.diffuseMap = test_t_2.name
+
 test_m_3 = Material.new("bricks")
-test_m_3.diffuseAlbedo[1] = 0.3
-test_m_3.diffuseAlbedo[2] = 0
-test_m_3.diffuseMap = test_t_1.name
+test_m_3.diffuseAlbedo[1] = 3.1
+test_m_3.diffuseAlbedo[2] = 3.2
+test_m_3.diffuseAlbedo[3] = 3.3
+test_m_3.diffuseAlbedo[4] = 3.4
+test_m_3.fresnelR[1] = 3.1
+test_m_3.fresnelR[2] = 3.2
+test_m_3.fresnelR[3] = 3.3
+test_m_3.roughness = 3.11
 
 test_m_4 = Material.new("tree")
-test_m_4.diffuseAlbedo[3] = 0.222223333
+test_m_4.diffuseAlbedo[1] = 4.1
+test_m_4.diffuseAlbedo[2] = 4.2
+test_m_4.diffuseAlbedo[3] = 4.3
+test_m_4.diffuseAlbedo[4] = 4.4
+test_m_4.fresnelR[1] = 4.1
+test_m_4.fresnelR[2] = 4.2
+test_m_4.fresnelR[3] = 4.3
+test_m_4.roughness = 4.11
 test_m_4.normalMap = test_t_1.name
 test_m_4.diffuseMap = test_t_2.name
 
