3a6b6eaebd27beda30749e9bb7fe7d7a72850053
181847 <753090084@qq.com>
2017-11-30 13:09:47 +0800

    在新项目中开始启动测试单元。

--- AssetsToolsUnitTest/AssetsToolsUnitTest.cpp
+++ AssetsToolsUnitTest/AssetsToolsUnitTest.cpp
@@ -4,10 +4,42 @@
 #include "stdafx.h"
 #include "../../Library/Lua/LuaInterpreter/LuaInterpreter.h"
 #include "../LoadAssets/AssetsTools.h"
+#include "../../Library/MyTools/UnitTestModules.h"
+
+// this variable is externed in 'Library/DirectX12/d3dUt'
+const int gNumFrameResources = 3;
+
+DECLARE_TEST_UNITS;
+
+static std::unique_ptr<Lua::LuaInterpreter> pLuaInter = std::make_unique<Lua::LuaInterpreter>();
+#define TEST_TARGET pLuaInter
+
+
+void TestUnit::GetReady()
+{
+	TEST_TARGET->DoFile("Init.lua");
+}
+
+void TestUnit::AfterTest()
+{
+	TEST_TARGET->Run();
+}
+
+void TestUnit::AddTestUnit()
+{
+	TEST_UNIT_START("start up, can you see me?")
+		return true;
+	TEST_UNIT_END;
+
+	TEST_UNIT_START("")
+
+	TEST_UNIT_END;
+}
 
 
 int main()
 {
+	TestUnit::testMain();
     return 0;
 }
 
