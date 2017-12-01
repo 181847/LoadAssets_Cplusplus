8608875f140b2196718501944b1be5d67e6f910b
181847 <753090084@qq.com>
2017-12-01 17:10:56 +0800

    增加两个宏用于将一个序号映射到xyz/w上，因为XMFLOAT只提供了xyzw的访问方法，很不方便。

--- LoadAssets/AssetsTools.h
+++ LoadAssets/AssetsTools.h
@@ -9,6 +9,52 @@
 
 #pragma comment(lib, "LuaInterpreter.lib")
 
+// some struct use x/y/z/w to store data,
+// and no index of array, this marco is to map the data of some index
+// to specific x/y/z/w,
+// receiver.{x, y, z, w}[index] = data,
+// the index start from zero.
+#define MAP_INDEX_TO_XYZW(data, index, receiver)\
+	switch(index)								\
+	{											\
+	case 0:										\
+		receiver.x = data;						\
+		break;									\
+	case 1:										\
+		receiver.y = data;						\
+		break;									\
+	case 2:										\
+		receiver.z = data;						\
+		break;									\
+	case 3:										\
+		receiver.w = data;						\
+		break;									\
+	default:									\
+		ASSERT(false && "map data to x//y//z//w failed ");	\
+	}											\
+// marco MAP_INDEX_TO_XYZW end
+
+// some struct use x/y/z to store data,
+// and no index of array, this marco is to map the data of some index
+// to specific x/y/z,
+// receiver.{x, y, z, w}[index] = data,
+// the index start from zero.
+#define MAP_INDEX_TO_XYZ(data, index, receiver)\
+	switch(index)								\
+	{											\
+	case 0:										\
+		receiver.x = data;						\
+		break;									\
+	case 1:										\
+		receiver.y = data;						\
+		break;									\
+	case 2:										\
+		receiver.z = data;						\
+		break;						\
+	default:									\
+		ASSERT(false && "map data to x//y//z failed ");	\
+	}											\
+// marco MAP_INDEX_TO_XYZ end
 
 namespace Lua
 {
